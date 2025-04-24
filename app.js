f01c80964e1277617606e6e7278a6f4510c9ec3f
ANANDHU KRISHNA CS <anandhukrishnacs@gmail.com>
2025-04-24 15:36:22 +0530

    24up

--- app.js
+++ app.js
@@ -10,12 +10,15 @@ var app=express();
 
 
 const userRoute=require("./routes/userRoute")
+const productRoute=require("./routes/productRoute")
 
 app.use(express.json())
 
 app.use(cors())
 
 app.use('/api',userRoute)
+app.use('/api',productRoute)
+
 
 app.listen(port,()=>{
     console.log(`Server is up and Running ${port}`)
